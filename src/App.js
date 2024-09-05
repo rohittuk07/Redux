0dc5905a27aa444ac02071dfa407cd3e1134c016
rohittuk07 <rohitchauhan93899@gmail.com>
2024-09-05 23:34:55 +0530

    redux project done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useEffect } from 'react'
 import Navbar from './Components/Navbar'
 import CartContainer from './Components/CartContainer'
+import { useSelector, useDispatch } from 'react-redux'
+import { calculateTotals} from './Features/Cart/CartSlice'
+
+
 
 const App = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar/>
--- src/Features/Cart/CartSlice.js
+++ src/Features/Cart/CartSlice.js
@@ -4,8 +4,9 @@ import cartItems from "../../cartItems";
 
 const initialState ={
     cartItems: cartItems,
-    amount:4,
-    total:1000,
+    amount:0,
+    total:0,
+    isloading: true,
 
 
 }
@@ -17,12 +18,38 @@ const cartSlice  =createSlice({
     reducers: {
        clearCart: (state) => {
         state.cartItems = [];
+       },
+       removeItem: (state, action) => {
+        const itemId = action.payload;
+        state.cartItems = state.cartItems.filter((item) => item.id !==itemId)
+
+       },
+       increase: (state, {payload}) =>{
+        const cartItem = state.cartItems.find((item) => item.id === payload.id);
+       cartItem.amount = cartItem.amount + 1;
+
+       },
+       decrease: (state, {payload}) => {
+        const cartItem = state.cartItems.find((item) => item.id === payload.id);
+       cartItem.amount = cartItem.amount - 1;
+
+
+       },
+       calculateTotals: (state) => {
+        let amount = 0;
+        let total = 0;
+        state.cartItems.forEach((item) => {
+          amount += item.amount;
+          total += item.price * item.amount;
+        })
+        state.amount =amount;
+        state.total = total;
        }
-        
+
     }
 })
 
 
 console.log(cartSlice)
+export const { clearCart,removeItem,increase,decrease , calculateTotals} = cartSlice.actions;
 export default cartSlice.reducer;
-export const { clearCart } = cartSlice.actions;
